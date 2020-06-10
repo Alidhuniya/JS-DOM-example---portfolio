c80605b94f9c54c22137ba436b28b3e27263feb9
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-06-09 17:48:23 -0700

    about me section added

--- main.js
+++ main.js
@@ -101,6 +101,16 @@ triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
 
 
 // change background-theme
+themeSetting = () => {
+  var x = document.getElementById("configure");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
  changeBgr = () => {
   document.body.style.backgroundColor = "#7CEC9F";
 }
@@ -113,11 +123,71 @@ changeBgr2 = () => {
   document.body.style.backgroundColor = "#FD79A8";
 }
 
-function themeSetting() {
-  var x = document.getElementById("configure");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+
+// Name type Writer
+
+
+class TypeWriter {
+  constructor(txtElement, words, wait = 1000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 5);
+      this.type();
+      this.isDeleting = false;
+  }
+
+  type() {
+
+      const current = this.wordIndex % this.words.length;
+
+      const fullTxt = this.words[current];
+
+
+      if (this.isDeleting) {
+
+          this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+
+          this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      let typeSpeed = 200;
+
+      if (this.isDeleting) {
+          typeSpeed /= 2;
+      }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+
+          typeSpeed = this.wait;
+
+          this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+          this.isDeleting = false;
+
+          this.wordIndex++;
+
+          typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
   }
 }
+
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+
+    new TypeWriter(txtElement, words, wait);
+}
+
+
